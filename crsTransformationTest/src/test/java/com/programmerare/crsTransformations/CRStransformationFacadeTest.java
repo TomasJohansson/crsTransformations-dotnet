4de29a13584fb9ee6c253712f9ac2c1fbe17aed8
Tomas Johansson <github-tomas@programmerare.com>
2018-05-21 00:19:02 +0200

    Added coordinate transforms test based on am added CSV file with known coordinates in three reference systems. Also added the dependency for JUnit 5 parameterized tests.

--- crsTransformationTest/src/test/java/com/programmerare/crsTransformations/CRStransformationFacadeTest.java
+++ crsTransformationTest/src/test/java/com/programmerare/crsTransformations/CRStransformationFacadeTest.java
@@ -2,15 +2,22 @@ package com.programmerare.crsTransformations;
 
 import com.programmerare.crsTransformationFacadeGooberCTL.CRStransformationFacadeGooberCTL;
 import com.programmerare.crsTransformationFacadeOrbisgisCTS.CRStransformationFacadeOrbisgisCTS;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 final class CRStransformationFacadeTest {
+
+    private final static int epsgNumberForWgs84 = 4326;
+    private final static int epsgNumberForRT90 = 3021;
+    private final static int epsgNumberForSweref99TM = 3006;
 
     private final static List<CRStransformationFacade> crsTransformationFacadeImplementations = Arrays.asList(
         new CRStransformationFacadeGooberCTL(),
@@ -45,8 +52,6 @@ final class CRStransformationFacadeTest {
         double sweref99_Y_expected = 6580822;
         double sweref99_X_expected = 674032;
 
-        int epsgNumberForWgs84 = 4326;
-        int epsgNumberForSweref99TM = 3006;
         Coordinate inputCoordinate = new Coordinate(wgs84Lon, wgs84Lat, epsgNumberForWgs84);
         Coordinate outputCoordinate = crsTransformationFacade.transform(inputCoordinate, epsgNumberForSweref99TM);
         assertEquals(sweref99_Y_expected, outputCoordinate.getYLatitude(), 0.5);
@@ -60,12 +65,79 @@ final class CRStransformationFacadeTest {
         double rt90_Y = 6580994;
         double rt90_X = 1628294;
 
-        int epsgNumberForWgs84 = 4326;
-        int epsgNumberForRT90 = 3021;
         Coordinate inputCoordinate = new Coordinate(rt90_X, rt90_Y, epsgNumberForRT90);
 
         Coordinate outputCoordinate = crsTransformationFacade.transform(inputCoordinate, epsgNumberForWgs84);
         assertEquals(wgs84Lat_expected, outputCoordinate.getYLatitude(), 0.1);
         assertEquals(wgs84Lon_expected, outputCoordinate.getXLongitude(), 0.1);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/coordinatesForSweden.csv", numLinesToSkip = 3, delimiter = ';')
+    void testWithCsvFileSource(
+        String description,
+        double wgs84Lat, double wgs84Lon,
+        double rt90nord, double rt90ost,
+        double sweref99nord, double sweref99ost,
+        String url
+    ) {
+        // example row from the csv file:
+        // Stockholm Centralstation;59.330231;18.059196;6580994;1628294;6580822;674032;https://kartor.eniro.se/m/03Yxp
+
+        // These used coordinates (i.e. those in the csv file) were manually retrieved from the Eniro
+        // site at the URL's for each row, and by clicking the coordinate feature
+        // which shows the coordinates in the three systems WGS84, RT90, SWREF99
+
+        for (CRStransformationFacade crsTransformationFacade : crsTransformationFacadeImplementations) {
+            testTransformation(crsTransformationFacade, epsgNumberForWgs84, epsgNumberForRT90, wgs84Lat, wgs84Lon, rt90nord, rt90ost, description);
+            testTransformation(crsTransformationFacade, epsgNumberForWgs84, epsgNumberForSweref99TM, wgs84Lat, wgs84Lon, sweref99nord, sweref99ost, description);
+            testTransformation(crsTransformationFacade, epsgNumberForRT90, epsgNumberForSweref99TM, rt90nord, rt90ost, sweref99nord, sweref99ost, description);
+        }
+    }
+
+    private void testTransformation(CRStransformationFacade crsTransformationFacade, int epsgNumber1, int epsgNumber2, double lat1, double lon1, double lat2, double lon2, String description) {
+        final Coordinate coordinate1 = new Coordinate(lon1, lat1, epsgNumber1);
+        final Coordinate coordinate2 = new Coordinate(lon2, lat2, epsgNumber2);
+        final Coordinate outputForCoordinate1 = crsTransformationFacade.transform(coordinate1, epsgNumber2);
+        final Coordinate outputForCoordinate2 = crsTransformationFacade.transform(coordinate2, epsgNumber1);
+
+        double delta = getDeltaValueForComparisons(epsgNumber2);
+        assertEquals(coordinate2.getXLongitude(), outputForCoordinate1.getXLongitude(), delta, description);
+        assertEquals(coordinate2.getYLatitude(), outputForCoordinate1.getYLatitude(), delta, description);
+
+        delta = getDeltaValueForComparisons(epsgNumber1);
+        assertEquals(coordinate1.getXLongitude(), outputForCoordinate2.getXLongitude(), delta, description);
+        assertEquals(coordinate1.getYLatitude(), outputForCoordinate2.getYLatitude(), delta, description);
+    }
+
+    private double getDeltaValueForComparisons(int epsgNumber) {
+        CoordinateReferenceSystemUnit coordinateReferenceSystemUnit = CoordinateReferenceSystemUnit.UNKNOWN;
+        if(epsgNumber == epsgNumberForWgs84) {
+            coordinateReferenceSystemUnit = CoordinateReferenceSystemUnit.DEGREES;
+        }
+        else if(epsgNumber == epsgNumberForRT90 ||epsgNumber == epsgNumberForSweref99TM) {
+            coordinateReferenceSystemUnit = CoordinateReferenceSystemUnit.METERS;
+        }
+        return getDeltaValueForComparisons(coordinateReferenceSystemUnit, epsgNumber);
+    }
+
+    private double getDeltaValueForComparisons(CoordinateReferenceSystemUnit coordinateReferenceSystemUnit, int epsgNumberUsedOnlyInErrorMessage) {
+        if(coordinateReferenceSystemUnit == CoordinateReferenceSystemUnit.DEGREES) {
+            // one of the results:
+            // Expected :20.266843
+            // Actual   :20.266853924440145
+            // the above diff is about 0.0001
+            return 0.0001;
+        }
+        else if(coordinateReferenceSystemUnit == CoordinateReferenceSystemUnit.METERS) {
+            // one of the results:
+            // Expected :386087.0
+            // Actual   :386088.0820856609
+            // the above diff is about 1.08
+            return 1.1;
+        }
+        else { // if(coordinateReferenceSystemUnit == CoordinateReferenceSystemUnit.UNKNOWN) {
+            throw new IllegalArgumentException("Not supported epsg number: " + epsgNumberUsedOnlyInErrorMessage);
+        }
     }
 }
