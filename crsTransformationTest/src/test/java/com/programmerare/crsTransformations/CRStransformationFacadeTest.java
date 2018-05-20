562361819fc9ac720e62dc9bcca97ee6981f83bc
Tomas Johansson <github-tomas@programmerare.com>
2018-05-20 23:09:33 +0200

    Removed "Base" from being a part of the class name since it is no longer a base class.

--- crsTransformationTest/src/test/java/com/programmerare/crsTransformations/CRStransformationFacadeTest.java
+++ crsTransformationTest/src/test/java/com/programmerare/crsTransformations/CRStransformationFacadeTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class CRStransformationFacadeBaseTest {
+final class CRStransformationFacadeTest {
 
     @DisplayName("Goober Cts transformation from WGS84 to SWEREF99")
     @Test
