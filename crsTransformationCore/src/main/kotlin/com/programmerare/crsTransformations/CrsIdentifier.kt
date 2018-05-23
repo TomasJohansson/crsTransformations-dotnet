ecb375450fcbb601d9fffe45a5e0fe4b97ba3e42
Tomas Johansson <github-tomas@programmerare.com>
2018-05-23 20:34:23 +0200

    Added two EPSG properties to CrsIdentifier, including tests. Also hamcrest dependency is added to build.gradle since it used in test code.

--- crsTransformationCore/src/main/kotlin/com/programmerare/crsTransformations/CrsIdentifier.kt
+++ crsTransformationCore/src/main/kotlin/com/programmerare/crsTransformations/CrsIdentifier.kt
@@ -1,17 +1,44 @@
 package com.programmerare.crsTransformations
 
-class CrsIdentifier private constructor(val crsCode: String) {
+class CrsIdentifier private constructor(val crsCode: String, val isEpsgCode: Boolean, val epsgNumber: Int) {
+
+    // The created crsCode strings will be trimmed, and if they are "epsg" (or e.g. "ePsG") then they will be uppercased i.e. "EPSG"
+
     companion object {
-        @JvmStatic
-        fun createFromCrsCode(crsCode: String): CrsIdentifier {
-            return CrsIdentifier(crsCode)
+        private const val EPSG_PREFIX_UPPERCASED = "EPSG:"
+
+        private val regexWithEpsgDigitsWithinTheOnlyParenthesis: Regex
+
+        init {
+            regexWithEpsgDigitsWithinTheOnlyParenthesis = Regex("""\s*epsg:(\d+)\s*""", RegexOption.IGNORE_CASE)
         }
 
-        private const val EPSG_PREFIX = "EPSG:"
+        @JvmStatic
+        fun createFromCrsCode(crsCode: String): CrsIdentifier {
+            // the three default values to uses, unless the input string will match
+            // a regular expression with EPSG string
+            var epsgNumber = -999999
+            var isEpsgCode = false
+            var crsIdentifierCode = crsCode.trim()
+            if(crsIdentifierCode.isNullOrEmpty()) {
+                throw java.lang.IllegalArgumentException("CRS code must be non-empty")
+            }
+
+            val matchGroupCollection: MatchGroupCollection? = regexWithEpsgDigitsWithinTheOnlyParenthesis.matchEntire(crsIdentifierCode)?.groups
+            if(matchGroupCollection != null) {
+                // as the name of the regexp indicates, digits should be matched within parenthesis
+                // which means that the below could should always return an integer
+                // and therefore "!!" should not cause any NullPointerException
+                epsgNumber = matchGroupCollection.get(1)?.value!!.toInt()
+                isEpsgCode = true
+                crsIdentifierCode =  EPSG_PREFIX_UPPERCASED + epsgNumber
+            }
+            return CrsIdentifier(crsIdentifierCode, isEpsgCode, epsgNumber)
+        }
 
         @JvmStatic
         fun createFromEpsgNumber(epsgNumber: Int): CrsIdentifier {
-            return CrsIdentifier(EPSG_PREFIX + epsgNumber)
+            return CrsIdentifier(crsCode =  EPSG_PREFIX_UPPERCASED + epsgNumber, isEpsgCode = true, epsgNumber = epsgNumber)
         }
     }
 }
--- crsTransformationCore/src/test/java/com/programmerare/crsTransformations/CrsIdentifierTest.java
+++ crsTransformationCore/src/test/java/com/programmerare/crsTransformations/CrsIdentifierTest.java
@@ -1,19 +1,49 @@
 package com.programmerare.crsTransformations;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CrsIdentifierTest {
 
     @Test
-    void createFromCrsCodeTest() {
-        CrsIdentifier crsIdentifier = CrsIdentifier.createFromCrsCode("abc");
+    void createFromCrsCodeTrimmedButWithoutUppercasingSinceNotEpsg() {
+        CrsIdentifier crsIdentifier = CrsIdentifier.createFromCrsCode("  abc  ");
         assertEquals("abc", crsIdentifier.getCrsCode());
+        assertEquals(false, crsIdentifier.isEpsgCode());
     }
 
     @Test
-    void createFromEpsgNumberTest() {
-        CrsIdentifier crsIdentifier = CrsIdentifier.createFromEpsgNumber(4321);
-        assertEquals("EPSG:4321", crsIdentifier.getCrsCode());
+    void createFromEpsgNumber() {
+        CrsIdentifier crsIdentifier = CrsIdentifier.createFromEpsgNumber(3006);
+        assertEquals("EPSG:3006", crsIdentifier.getCrsCode());
+        assertEquals(true, crsIdentifier.isEpsgCode());
+        assertEquals(3006, crsIdentifier.getEpsgNumber());
+    }
+
+    @Test
+    void createFromCrsCodeWithUppercasingSinceEpsg() {
+        CrsIdentifier crsIdentifier = CrsIdentifier.createFromCrsCode("  epsg:4326  ");
+        assertEquals("EPSG:4326", crsIdentifier.getCrsCode());
+        assertEquals(true, crsIdentifier.isEpsgCode());
+        assertEquals(4326, crsIdentifier.getEpsgNumber());
+    }
+
+    @Test
+    void createFromCrsCodeNull() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> CrsIdentifier.createFromCrsCode(null), "Must not be null");
+        // the exception message is currently something like this: "Parameter specified as non-null is null: method com.programmerare.crsTransformations.CrsIdentifier$Companion.createFromCrsCode, parameter crsCode"
+        // (potentially fragile to test the message strings but it does not really change often, and in such a rare scenario, then easy to fix)
+        assertThat(exception.getMessage(), containsString("non-null"));
+    }
+
+    @Test
+    void createFromCrsCodeEmpty() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> CrsIdentifier.createFromCrsCode("   "), "Must not be empty string");
+        // Fragile to test the message string below but easy to fix here if it would break, and it will not break/change often.
+        assertThat(exception.getMessage(), containsString("non-empty"));
     }
 }
