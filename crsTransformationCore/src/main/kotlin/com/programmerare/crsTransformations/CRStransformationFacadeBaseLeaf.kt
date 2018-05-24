c5fa7fdd91307082a38aa7d491c5452cda8dbebb
Tomas Johansson <github-tomas@programmerare.com>
2018-05-24 11:43:54 +0200

    Refactoring. Extracted four convenience methods from CRStransformationFacadeBaseLeaf into a base class CRStransformationFacadeBase which is now also base class for CRStransformationFacadeBaseComposite.

--- crsTransformationCore/src/main/kotlin/com/programmerare/crsTransformations/CRStransformationFacadeBaseLeaf.kt
+++ crsTransformationCore/src/main/kotlin/com/programmerare/crsTransformations/CRStransformationFacadeBaseLeaf.kt
@@ -1,24 +1,6 @@
 package com.programmerare.crsTransformations
 
-abstract class CRStransformationFacadeBaseLeaf : CRStransformationFacade {
-    override final fun transform(inputCoordinate: Coordinate, crsCodeForOutputCoordinateSystem: String): Coordinate {
-        // the below invoked overloaded method is implemented in subclasses
-        // i.e. it is a hook method invoked by this Template Method
-        return transform(inputCoordinate, CrsIdentifier.createFromCrsCode(crsCodeForOutputCoordinateSystem))
-    }
-
-    override final fun transform(inputCoordinate: Coordinate, epsgNumberForOutputCoordinateSystem: Int): Coordinate {
-        // this Template Method is invokign the below overloaded hook method in subclasses
-        return transform(inputCoordinate, CrsIdentifier.createFromEpsgNumber(epsgNumberForOutputCoordinateSystem))
-    }
-
-    override final fun transformToResultObject(inputCoordinate: Coordinate, epsgNumberForOutputCoordinateSystem: Int): TransformResult {
-        return transformToResultObject(inputCoordinate, CrsIdentifier.createFromEpsgNumber(epsgNumberForOutputCoordinateSystem))
-    }
-
-    override final fun transformToResultObject(inputCoordinate: Coordinate, crsCodeForOutputCoordinateSystem: String): TransformResult {
-        return transformToResultObject(inputCoordinate, CrsIdentifier.createFromCrsCode(crsCodeForOutputCoordinateSystem))
-    }
+abstract class CRStransformationFacadeBaseLeaf : CRStransformationFacadeBase(), CRStransformationFacade {
 
     override final fun transformToResultObject(inputCoordinate: Coordinate, crsIdentifierForOutputCoordinateSystem: CrsIdentifier): TransformResult {
         try {
--- crsTransformationCore/src/main/kotlin/com/programmerare/crsTransformations/compositeTransformations/CRStransformationFacadeBaseComposite.kt
+++ crsTransformationCore/src/main/kotlin/com/programmerare/crsTransformations/compositeTransformations/CRStransformationFacadeBaseComposite.kt
@@ -1,31 +1,13 @@
 package com.programmerare.crsTransformations.compositeTransformations
 
-import com.programmerare.crsTransformations.CRStransformationFacade
-import com.programmerare.crsTransformations.Coordinate
-import com.programmerare.crsTransformations.CrsIdentifier
-import com.programmerare.crsTransformations.TransformResult
+import com.programmerare.crsTransformations.*
 
 // subclasses:
 // CRStransformationFacadeChainOfResponsibility
 // CRStransformationFacadeAverage
-abstract class CRStransformationFacadeBaseComposite(private val crsTransformationFacades: java.util.List<CRStransformationFacade>) : CRStransformationFacade {
-    override fun transform(inputCoordinate: Coordinate, epsgNumberForOutputCoordinateSystem: Int): Coordinate {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun transform(inputCoordinate: Coordinate, crsCodeForOutputCoordinateSystem: String): Coordinate {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+abstract class CRStransformationFacadeBaseComposite(private val crsTransformationFacades: java.util.List<CRStransformationFacade>) : CRStransformationFacadeBase(), CRStransformationFacade {
 
     override fun transform(inputCoordinate: Coordinate, crsIdentifierForOutputCoordinateSystem: CrsIdentifier): Coordinate {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun transformToResultObject(inputCoordinate: Coordinate, epsgNumberForOutputCoordinateSystem: Int): TransformResult {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun transformToResultObject(inputCoordinate: Coordinate, crsCodeForOutputCoordinateSystem: String): TransformResult {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
