441718db57c649b842d83d566beed239b185b49d
Tomas Johansson <github-tomas@programmerare.com>
2018-10-07 23:27:50 +0200

    Moved three test classes from module "Core" to module "Test". Also removed some small unneeded classes from Core module (related to constants and constants tests). Now there are no tests located in Core but all tests are instead in Test which are not only testing Core but also other modules.

---
+++ crsTransformationTest/src/test/java/com/programmerare/crsTransformations/CrsIdentifierTest.java
@@ -0,0 +1,80 @@
+package com.programmerare.crsTransformations;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.programmerare.crsConstants.constantsByNumberNameArea.v9_5_4.EpsgNumber;
+import com.programmerare.crsConstants.constantsByNumberNameArea.v9_5_4.EpsgCode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CrsIdentifierTest {
+
+    @Test
+    void createFromCrsCodeTrimmedButWithoutUppercasingButNotEpsg() {
+        CrsIdentifier crsIdentifier = CrsIdentifier.createFromCrsCode("  abc  ");
+        assertEquals("abc", crsIdentifier.getCrsCode());
+        assertEquals(false, crsIdentifier.isEpsgCode());
+    }
+
+    @Test
+    void createFromEpsgNumber() {
+        // SWEREF99TM = 3006
+        CrsIdentifier crsIdentifier = CrsIdentifier.createFromEpsgNumber(3006);
+        Assertions.assertEquals(EpsgCode._3006__SWEREF99_TM__SWEDEN, crsIdentifier.getCrsCode());
+        assertEquals(true, crsIdentifier.isEpsgCode());
+        Assertions.assertEquals(EpsgNumber._3006__SWEREF99_TM__SWEDEN, crsIdentifier.getEpsgNumber());
+    }
+
+    @Test
+    void createFromCrsCodeWithLowerCasedEpsg() {
+        CrsIdentifier crsIdentifier = CrsIdentifier.createFromCrsCode("  epsg:4326  ");
+        // the input should become trimmed and uppercased "EPSG"
+        assertEquals("EPSG:4326", crsIdentifier.getCrsCode());
+        assertEquals(true, crsIdentifier.isEpsgCode());
+        assertEquals(4326, crsIdentifier.getEpsgNumber());
+    }
+
+    @Test
+    void createFromCrsCodeNullShouldFail() {
+        IllegalArgumentException exception = assertThrows(
+            IllegalArgumentException.class,
+            () -> CrsIdentifier.createFromCrsCode(null), // should fail
+            "Must not be null"
+        );
+        assertExceptionMessageWhenArgumentWasNullOrEmptyString(exception, "non-null");
+    }
+
+    @Test
+    void createFromCrsCodeWithOnlyWhiteSpaceShouldFail() {
+        IllegalArgumentException exception = assertThrows(
+            IllegalArgumentException.class,
+            () -> CrsIdentifier.createFromCrsCode("   "), // should fail
+            "Must not be empty string"
+        );
+        assertExceptionMessageWhenArgumentWasNullOrEmptyString(exception, "non-empty");
+    }
+
+    @Test
+    void equalsTestWhenCreatingFromEpsgNumberAndFromCrsCode() {
+        CrsIdentifier fromEpsgNumber    = CrsIdentifier.createFromEpsgNumber(3006);
+        CrsIdentifier fromCrsCode       = CrsIdentifier.createFromCrsCode("  epsg:3006   ");
+        assertEquals(fromEpsgNumber, fromCrsCode);
+        assertEquals(fromEpsgNumber.hashCode(), fromCrsCode.hashCode());
+    }
+
+    /**
+     * @param exception
+     * @param expectedStringToBeContainedInExceptionMessage e.g. "non-null" or "non-empty"
+     */
+    private void assertExceptionMessageWhenArgumentWasNullOrEmptyString(
+        IllegalArgumentException exception,
+        String expectedStringToBeContainedInExceptionMessage
+    ) {
+        // the exception message is currently something like this: "Parameter specified as non-null is null: method com.programmerare.crsTransformations.CrsIdentifier$Companion.createFromCrsCode, parameter crsCode"
+        // (potentially fragile to test the message strings but it does not really change often, and in such a rare scenario, then easy to fix)
+        assertThat(exception.getMessage(), containsString(expectedStringToBeContainedInExceptionMessage));
+    }
+}
---
+++ crsTransformationTest/src/test/java/com/programmerare/crsTransformations/TransformResultImplementationTest.java
@@ -0,0 +1,115 @@
+package com.programmerare.crsTransformations;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TransformResultImplementationTest {
+
+    private TransformResult transformResult;
+    private Coordinate inputCoordinate;
+    private Coordinate outputCoordinate;
+
+    private CrsTransformationFacade crsTransformationFacade;
+
+    @BeforeEach
+    void beforeEach() {
+        inputCoordinate = Coordinate.createFromXLongYLat(0.0, 0.0, 1234);
+        outputCoordinate = inputCoordinate;
+
+        crsTransformationFacade = new CrsTransformationFacadeTemp();
+    }
+
+    @Test
+    void transformNotSucessTest() {
+        transformResult = new TransformResultImplementation(inputCoordinate, null, null, false, crsTransformationFacade);
+        assertNotNull(transformResult);
+        assertFalse(transformResult.isSuccess());
+    }
+
+    @Test
+    void transformSucessTest() {
+        transformResult = new TransformResultImplementation(inputCoordinate, outputCoordinate, null, true, crsTransformationFacade);
+        assertNotNull(transformResult);
+        assertTrue(transformResult.isSuccess());
+    }
+
+    @Test
+    void unvalidTransformResultConstructionWhenSuccessIsFalseButCoordinateProvided() {
+        assertThrows(
+            IllegalStateException.class,
+            () -> new TransformResultImplementation(inputCoordinate, outputCoordinate, null, false, crsTransformationFacade),
+            "unvalid TransformResult object construction should throw exception"
+        );
+    }
+
+    @Test
+    void unvalidTransformResultConstructionWhenSuccessIsTrueButCoordinateNotProvided() {
+        outputCoordinate = null;
+        assertThrows(
+            IllegalStateException.class,
+            () -> new TransformResultImplementation(inputCoordinate, outputCoordinate, null, true, crsTransformationFacade),
+            "unvalid TransformResult object construction should throw exception"
+        );
+    }
+
+    @Test
+    void preconditionViolationWhenTryingToAccessCordinateWhenSuccessFalse() {
+        outputCoordinate = null;
+        transformResult = new TransformResultImplementation(inputCoordinate, outputCoordinate, null, false, crsTransformationFacade);
+        Exception e = assertThrows(
+            RuntimeException.class,
+            () -> transformResult.getOutputCoordinate(),
+            "should not try to get output coordinate unless the result was a success"
+        );
+        String exceptionMessage = e.getMessage().toLowerCase().replaceAll("-", "");
+        // the purpose of ther above is to make the test less sensitive to the exact message
+        // e.g. will match text containing "Pre-condition" (including hyphen and uppercased first letter)
+        assertThat(exceptionMessage, containsString("precondition"));
+    }
+
+    // TODO elimitate the class below
+    class CrsTransformationFacadeTemp implements CrsTransformationFacade {
+        @NotNull
+        @Override
+        public Coordinate transform(@NotNull Coordinate inputCoordinate, int epsgNumberForOutputCoordinateSystem) {
+            return null;
+        }
+
+        @NotNull
+        @Override
+        public Coordinate transform(@NotNull Coordinate inputCoordinate, @NotNull String crsCodeForOutputCoordinateSystem) {
+            return null;
+        }
+
+        @NotNull
+        @Override
+        public Coordinate transform(@NotNull Coordinate inputCoordinate, @NotNull CrsIdentifier crsIdentifierForOutputCoordinateSystem) {
+            return null;
+        }
+
+        @NotNull
+        @Override
+        public TransformResult transformToResultObject(@NotNull Coordinate inputCoordinate, int epsgNumberForOutputCoordinateSystem) {
+            return null;
+        }
+
+        @NotNull
+        @Override
+        public TransformResult transformToResultObject(@NotNull Coordinate inputCoordinate, @NotNull String crsCodeForOutputCoordinateSystem) {
+            return null;
+        }
+
+        @NotNull
+        @Override
+        public TransformResult transformToResultObject(@NotNull Coordinate inputCoordinate, @NotNull CrsIdentifier crsIdentifierForOutputCoordinateSystem) {
+            return null;
+        }
+    }
+}
+
