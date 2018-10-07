4493dd90b5363354a028872f9f34536c3474d5ef
Tomas Johansson <github-tomas@programmerare.com>
2018-10-07 20:46:41 +0200

    Refactored CrsIdentifier. The main change is removed usage of regular expression since a simple substring should have better performance

--- crsTransformationCore/src/main/kotlin/com/programmerare/crsTransformations/CrsIdentifier.kt
+++ crsTransformationCore/src/main/kotlin/com/programmerare/crsTransformations/CrsIdentifier.kt
@@ -1,44 +1,47 @@
 package com.programmerare.crsTransformations
 
-data class CrsIdentifier private constructor(val crsCode: String, val isEpsgCode: Boolean, val epsgNumber: Int) {
-
-    // The created crsCode strings will be trimmed, and if they are "epsg" (or e.g. "ePsG") then they will be uppercased i.e. "EPSG"
+data class CrsIdentifier private constructor(
+    val crsCode: String,
+    val isEpsgCode: Boolean,
+    val epsgNumber: Int
+) {
 
     companion object {
         private const val EPSG_PREFIX_UPPERCASED = "EPSG:"
+        private const val LENGTH_OF_EPSG_PREFIX = EPSG_PREFIX_UPPERCASED.length
 
-        private val regexWithEpsgDigitsWithinTheOnlyParenthesis: Regex
-
-        init {
-            regexWithEpsgDigitsWithinTheOnlyParenthesis = Regex("""\s*epsg:(\d+)\s*""", RegexOption.IGNORE_CASE)
-        }
-
+        // The crsCode string will become trimmed, and if it is
+        // "epsg" (or e.g. something like "ePsG") then it will be uppercased i.e. "EPSG"
         @JvmStatic
         fun createFromCrsCode(crsCode: String): CrsIdentifier {
-            // the three default values to uses, unless the input string will match
-            // a regular expression with EPSG string
-            var epsgNumber = -999999
+            // these two default values will be used, unless the crsCode parameter is an EPSG string
+            var epsgNumber = 0
             var isEpsgCode = false
-            var crsIdentifierCode = crsCode.trim()
-            if(crsIdentifierCode.isNullOrEmpty()) {
+
+            if(crsCode.isNullOrBlank()) {
                 throw java.lang.IllegalArgumentException("CRS code must be non-empty")
             }
+            var crsIdentifierCode = crsCode.trim() // but does not uppercase here (unless EPSG below)
 
-            val matchGroupCollection: MatchGroupCollection? = regexWithEpsgDigitsWithinTheOnlyParenthesis.matchEntire(crsIdentifierCode)?.groups
-            if(matchGroupCollection != null) {
-                // as the name of the regexp indicates, digits should be matched within parenthesis
-                // which means that the below could should always return an integer
-                // and therefore "!!" should not cause any NullPointerException
-                epsgNumber = matchGroupCollection.get(1)?.value!!.toInt()
-                isEpsgCode = true
-                crsIdentifierCode =  EPSG_PREFIX_UPPERCASED + epsgNumber
+            if(crsIdentifierCode.toUpperCase().startsWith(EPSG_PREFIX_UPPERCASED)) {
+                val nonEpsgPartOfString = crsIdentifierCode.substring(LENGTH_OF_EPSG_PREFIX);
+                val epsgNumberOrNull = nonEpsgPartOfString.toIntOrNull()
+                if(epsgNumberOrNull != null) {
+                    epsgNumber = epsgNumberOrNull
+                    isEpsgCode = true
+                    crsIdentifierCode = crsIdentifierCode.toUpperCase()
+                }
             }
             return CrsIdentifier(crsIdentifierCode, isEpsgCode, epsgNumber)
         }
 
         @JvmStatic
         fun createFromEpsgNumber(epsgNumber: Int): CrsIdentifier {
-            return CrsIdentifier(crsCode =  EPSG_PREFIX_UPPERCASED + epsgNumber, isEpsgCode = true, epsgNumber = epsgNumber)
+            return CrsIdentifier(
+                crsCode =  EPSG_PREFIX_UPPERCASED + epsgNumber,
+                isEpsgCode = true,
+                epsgNumber = epsgNumber
+            )
         }
     }
 }
