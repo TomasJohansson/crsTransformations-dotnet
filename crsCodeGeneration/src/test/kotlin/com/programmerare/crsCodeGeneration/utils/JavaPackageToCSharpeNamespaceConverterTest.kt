2eacd146f050cb67e6d6f4afe626b5078cfb45c9
Tomas Johansson <github-tomas@programmerare.com>
2018-10-28 10:42:44 +0100

    Minor refactoring of a test method

--- crsCodeGeneration/src/test/kotlin/com/programmerare/crsCodeGeneration/utils/JavaPackageToCSharpeNamespaceConverterTest.kt
+++ crsCodeGeneration/src/test/kotlin/com/programmerare/crsCodeGeneration/utils/JavaPackageToCSharpeNamespaceConverterTest.kt
@@ -6,7 +6,11 @@ import org.junit.jupiter.api.Assertions.*
 internal class JavaPackageToCSharpeNamespaceConverterTest {
     @Test
     fun getAsNameOfCSharpeNameSpace() {
-        val nameOfNamespace = JavaPackageToCSharpeNamespaceConverter.getAsNameOfCSharpeNameSpace("com.programmerare.crsConstants.constantsByAreaNameNumber.v9_5_4")
-        assertEquals("Programmerare.CrsConstants.ConstantsByAreaNameNumber.v9_5_4", nameOfNamespace)
+        assertEquals(
+            "Programmerare.CrsConstants.ConstantsByAreaNameNumber.v9_5_4",
+            JavaPackageToCSharpeNamespaceConverter.getAsNameOfCSharpeNameSpace(
+                "com.programmerare.crsConstants.constantsByAreaNameNumber.v9_5_4"
+            )
+        )
     }
 }
