0c58d6d7d7fef34031320ae17fce28fda7c49586
Tomas Johansson <github-tomas@programmerare.com>
2018-10-30 21:15:49 +0100

    Refactored duplicated code to a base class, with the only difference as a function parameter.

--- crsTransformationCore/src/main/kotlin/com/programmerare/crsTransformations/compositeTransformations/CompositeStrategyBase.kt
+++ crsTransformationCore/src/main/kotlin/com/programmerare/crsTransformations/compositeTransformations/CompositeStrategyBase.kt
@@ -1,7 +1,6 @@
 package com.programmerare.crsTransformations.compositeTransformations
 
-import com.programmerare.crsTransformations.Coordinate
-import com.programmerare.crsTransformations.CrsTransformationFacade
+import com.programmerare.crsTransformations.*
 
 internal abstract class CompositeStrategyBase
     (private val crsTransformationFacades: List<CrsTransformationFacade>)
@@ -10,4 +9,44 @@ internal abstract class CompositeStrategyBase
     override final fun getAllTransformationFacadesInTheOrderTheyShouldBeInvoked(): List<CrsTransformationFacade> {
         return crsTransformationFacades
     }
+
+    /**
+     * This base class method is reusable from both subclasses
+     * that calculates the median or average which is provided with the last
+     * function parameter of the method
+     */
+    protected fun calculateAggregatedResultBase(
+        allResults: List<TransformResult>,
+        inputCoordinate: Coordinate,
+        crsTransformationFacadeThatCreatedTheResult: CrsTransformationFacade,
+        medianOrAverage: () -> Coordinate
+    ): TransformResult {
+        val resultsStatistic = ResultsStatistic(allResults)
+        // TODO: pass the instance of the above resultsStatistic to the TransformResultImplementation
+        // instead of letting it create a new ... since it will become lazy load here regarding the average value
+        if(resultsStatistic.isStatisticsAvailable()) {
+            //  val coordRes = resultsStatistic.getCoordinateMean() // THE ONLY DIFFERENCE in the above mentioned two classes
+            //  val coordRes = resultsStatistic.getCoordinateAverage()  // THE ONLY DIFFERENCE in the above mentioned two classes
+            val coordRes: Coordinate = medianOrAverage() // this line replaced the above two lines in different subclasses when doing refactoring
+            return TransformResultImplementation(
+                inputCoordinate,
+                outputCoordinate = coordRes,
+                exception = null,
+                isSuccess = true,
+                crsTransformationFacadeThatCreatedTheResult = crsTransformationFacadeThatCreatedTheResult,
+                subResults = allResults
+            )
+        }
+        else {
+            return TransformResultImplementation(
+                inputCoordinate,
+                outputCoordinate = null,
+                exception = null,
+                isSuccess = false,
+                crsTransformationFacadeThatCreatedTheResult = crsTransformationFacadeThatCreatedTheResult,
+                subResults = allResults
+            )
+        }
+    }
+
 }
--- crsTransformationCore/src/main/kotlin/com/programmerare/crsTransformations/compositeTransformations/CompositeStrategyForAverageValue.kt
+++ crsTransformationCore/src/main/kotlin/com/programmerare/crsTransformations/compositeTransformations/CompositeStrategyForAverageValue.kt
@@ -17,30 +17,11 @@ internal class CompositeStrategyForAverageValue(
         crsTransformationFacadeThatCreatedTheResult: CrsTransformationFacade
     ): TransformResult {
         val resultsStatistic = ResultsStatistic(allResults)
-        // TODO: pass the instance of the above resultsStatistic to the TransformResultImplementation
-        // instead of letting it create a new ... since it will become lazy load here regarding the average value
-
-        // TOOD: reuse the below code which is VERY similar in CompositeStrategyForAverageValue and CompositeStrategyForMedianValue
-        if(resultsStatistic.isStatisticsAvailable()) {
-            val coordRes = resultsStatistic.getCoordinateAverage()  // THE ONLY DIFFERENCE in the above mentioned two classes
-            return TransformResultImplementation(
-                inputCoordinate,
-                outputCoordinate = coordRes,
-                exception = null,
-                isSuccess = true,
-                crsTransformationFacadeThatCreatedTheResult = crsTransformationFacadeThatCreatedTheResult,
-                subResults = allResults
-            )
-        }
-        else {
-            return TransformResultImplementation(
-                inputCoordinate,
-                outputCoordinate = null,
-                exception = null,
-                isSuccess = false,
-                crsTransformationFacadeThatCreatedTheResult = crsTransformationFacadeThatCreatedTheResult,
-                subResults = allResults
-            )
-        }
+        return this.calculateAggregatedResultBase(
+            allResults,
+            inputCoordinate,
+            crsTransformationFacadeThatCreatedTheResult,
+            { resultsStatistic.getCoordinateAverage() }
+        )
    }
 }
