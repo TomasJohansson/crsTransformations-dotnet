e696b68041097a6ee81325cf2a78c213f64e0d07
Tomas Johansson <github-tomas@programmerare.com>
2018-11-19 00:16:59 +0100

    Deleted many of the files which will not be used in the .NET solution

--- crs-transformation-code-generation/src/main/kotlin/com/programmerare/crsCodeGeneration/constantsGenerator/DataType.kt
+++
@@ -1,7 +0,0 @@
-package com.programmerare.crsCodeGeneration.constantsGenerator
-
-enum class DataType(val dataTypeName: String) {
-    INTEGER("int"),
-    STRING_JAVA("String"), // uppercased first letter
-    STRING_CSHARPE("string") // lowercased first letter
-}
