e7d9e37eb15944aa30a394d420555857a3467678
Tomas Johansson <github-tomas@programmerare.com>
2018-11-19 01:21:35 +0100

    Added currently latest NUnit files (version 3.11.0) with NuGet, and also the latest "Microsoft.NET.Test.Sdk". Also implemented a trivial test just to verify the setup/installation of NUnit.

--- Programmerare.CrsTransformations.Test/Class1.cs
+++ Programmerare.CrsTransformations.Test/Class1.cs
@@ -1,8 +1,13 @@
-﻿using System;
-
+﻿using NUnit.Framework;
 namespace Programmerare.CrsTransformations.Test
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void FirstTest()
+        {
+            Assert.AreEqual(0, 0);
+        }
     }
 }
