e5936db801807d95c305959c6283200a385ec410
Tomas Johansson <github-tomas@programmerare.com>
2018-11-20 12:12:49 +0100

    Moved the file with F# EPSG contants (generated by the JVM/Kotlin/Java project) into the recently crelated F# project. Also added a few C# test to verify it works as expected.

---
+++ Programmerare.CrsTransformations.Test/CrsConstants/EpsgNumberTest.cs
@@ -0,0 +1,37 @@
+﻿using Programmerare.CrsConstants.ConstantsByAreaNameNumber.v9_5_4;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+namespace Programmerare.CrsTransformations.Test.CrsConstants
+{
+    [TestFixture]
+    class EpsgNumberTest
+    {
+        [Test]
+        public void WGS_84()
+        {
+            Assert.AreEqual(
+                4326,
+                EpsgNumber.WORLD__WGS_84__4326
+            );
+        }
+        [Test]
+        public void SWEREF99_TM()
+        {
+            Assert.AreEqual(
+                3006,
+                EpsgNumber.SWEDEN__SWEREF99_TM__3006
+            );
+        }
+        [Test]
+        public void RT90_2_5_GON_V()
+        {
+            Assert.AreEqual(
+                3021,
+                EpsgNumber.SWEDEN__2_5_GON_W__RT90_2_5_GON_V__3021
+            );
+        }
+
+    }
+}
