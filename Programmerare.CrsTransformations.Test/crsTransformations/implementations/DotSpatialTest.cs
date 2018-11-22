de209620bef5d51dc941efc635289cd5a9bf6f66
Tomas Johansson <github-tomas@programmerare.com>
2018-11-23 00:44:15 +0100

    Upgraded DotSpatial from version 1.9.0 (2016-04-28) to version 2.0.0-rc1 (2017-03-11). This upgrade fixed a problem with the tested Swedish coordinates. For further information, see the comments in the commited test file.

--- Programmerare.CrsTransformations.Test/crsTransformations/implementations/DotSpatialTest.cs
+++ Programmerare.CrsTransformations.Test/crsTransformations/implementations/DotSpatialTest.cs
@@ -1,11 +1,26 @@
 ﻿using Programmerare.CrsTransformations.Adapter.DotSpatial;
 using NUnit.Framework;
 
+// https://www.nuget.org/packages/DotSpatial.Projections
+// The current (as of november 2018) latest versions:
+// 2.0.0-rc1    2017-03-11
+// 1.9.0        2016-04-28
+
+// Version 1.9.0 caused problems (e.g. in this test class below) with very 
+// bad results for some Swedish coordinate reference systems.
+// Therefore the release candadidate is currently used.
+// The problem was fixed in october 2016 
+// (i.e. about half a year after relase 1.9.0)
+// https://github.com/DotSpatial/DotSpatial/pull/865
+
 namespace Programmerare.CrsTransformations.Test.Implementations
 {
     [TestFixture]
     class DotSpatialTest : AdaptersTestBase
     {
+        // The below comment does not apply to DotSpatial anymore 
+        // after the upgrade 2.0.0-rc1 as commented above.
+
         // The two implementations DotSpatial and ProjNet4GeoAPI
         // (DotSpatial is tested below, and ProjNet4GeoAPI from another test class)
         // are currently producing similarly bad results when transforming 
@@ -34,13 +49,20 @@ namespace Programmerare.CrsTransformations.Test.Implementations
                 // that I have "cheated" with high values 
                 // to get "succeeding" tests ...
 
-                195, // maxMeterDifferenceForSuccessfulTest
+                // DotSpatial 2.0.0-rc1:
+                0.5, // maxMeterDifferenceForSuccessfulTest
+                // DotSpatial 1.9.0:
+                // 195, // maxMeterDifferenceForSuccessfulTest
+
                 // Note that one of the test fails if the above value 
                 // is changed to 190 meters !
                 // Yes METERS ! i.e. this is not very accurate results 
                 // with the current DotSpatial implementation for the Swedish CRS used
 
-                0.01 // maxLatLongDifferenceForSuccessfulTest
+                // DotSpatial 2.0.0-rc1:
+                0.00001 // maxLatLongDifferenceForSuccessfulTest
+                // DotSpatial 1.9.0:
+                // 0.01 // maxLatLongDifferenceForSuccessfulTest
             );
         }
     }
