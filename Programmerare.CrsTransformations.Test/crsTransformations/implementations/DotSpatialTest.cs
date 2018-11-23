3314d5f1e09697345540f5da859bcf025c6996a3
Tomas Johansson <github-tomas@programmerare.com>
2018-11-23 01:09:34 +0100

    Only some comments.

--- Programmerare.CrsTransformations.Test/crsTransformations/implementations/DotSpatialTest.cs
+++ Programmerare.CrsTransformations.Test/crsTransformations/implementations/DotSpatialTest.cs
@@ -18,46 +18,21 @@ namespace Programmerare.CrsTransformations.Test.Implementations
     [TestFixture]
     class DotSpatialTest : AdaptersTestBase
     {
-        // The below comment does not apply to DotSpatial anymore 
-        // after the upgrade 2.0.0-rc1 as commented above.
-
-        // The two implementations DotSpatial and ProjNet4GeoAPI
-        // (DotSpatial is tested below, and ProjNet4GeoAPI from another test class)
-        // are currently producing similarly bad results when transforming 
-        // to the Swedish CRS "RT90 2.5 gon V"
-        // Then it may seem reasonable to suspect that those 
-        // two are actually correct, but the reason for considering them to 
-        // be non-accurate is the fact that the tested coordinates
-        // (which are defined in the above inherited base class)
-        // have been retrieved with FIVE different websites
-        // (among others the two websites hitta.se and Eniro which are 
-        //   very well-known and visited websites for Swedes
-        //   and those two should really have made sure to implement 
-        //   correct transformation to the two Swedish coordinate reference systems RT90 and SWEREF99)
-
         [SetUp]
         public void SetUp()
         {
             base.SetUpbase(
                 new CrsTransformationAdapterDotSpatial(),
                 CrsTransformationAdapteeType.LEAF_DOT_SPATIAL_1_9_0,
-                
-                // It is questionable where to draw the limit 
-                // between a failing and succeeding test 
-                // when the result is not accurate,
-                // and here below I it is indeed reasonable to claim 
-                // that I have "cheated" with high values 
-                // to get "succeeding" tests ...
 
                 // DotSpatial 2.0.0-rc1:
                 0.5, // maxMeterDifferenceForSuccessfulTest
                 // DotSpatial 1.9.0:
                 // 195, // maxMeterDifferenceForSuccessfulTest
-
-                // Note that one of the test fails if the above value 
-                // is changed to 190 meters !
-                // Yes METERS ! i.e. this is not very accurate results 
-                // with the current DotSpatial implementation for the Swedish CRS used
+                // Note that one of the tests fails (for DotSpatial 1.9.0) 
+                // if the above value was changed to 190 meters !
+                // But the problem is fixed with version 2.0.0-rc1
+                // https://github.com/DotSpatial/DotSpatial/pull/865
 
                 // DotSpatial 2.0.0-rc1:
                 0.00001 // maxLatLongDifferenceForSuccessfulTest
--- Programmerare.CrsTransformations.Test/crsTransformations/implementations/ProjNet4GeoAPITest.cs
+++ Programmerare.CrsTransformations.Test/crsTransformations/implementations/ProjNet4GeoAPITest.cs
@@ -12,11 +12,12 @@ namespace Programmerare.CrsTransformations.Test.Implementations
                 new CrsTransformationAdapterProjNet4GeoAPI(),
                 CrsTransformationAdapteeType.LEAF_PROJ_NET_4_GEO_API_1_4_1,
 
-                // See the comment in "DotSpatialTest"
-                // regarding the non-accurate result for DotSpatial
-                // and similar non-accurate results for ProjNet4GeoAPI
-                // with the below HIGH delta values
-                // (required to avoid failing/red test)
+                // The implementation ProjNet4GeoAPI
+                // is currently (version 1.4.1) 
+                // producing bad results when transforming 
+                // to the Swedish CRS "RT90 2.5 gon V"
+                // https://github.com/NetTopologySuite/ProjNet4GeoAPI/issues/38
+
 
                 // It is questionable where to draw the limit 
                 // between a failing and succeeding test 
